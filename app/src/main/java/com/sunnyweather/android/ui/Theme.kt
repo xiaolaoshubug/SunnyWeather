86e29801c91824422f3bac2d9cb5cba7087bada2
aoyio <aoyio@qq.com>
2021-11-26 09:05:09 +0800

    1、配置网络请求权限属性、判断是否支持暗黑主题

---
+++ app/src/main/java/com/sunnyweather/android/ui/Theme.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android.ui
+
+import android.content.Context
+import android.content.res.Configuration
+
+/**
+ * Author AY
+ * Version 1.0
+ * Date 2021/11/25
+ * 判断当前是暗黑主题还是浅色主题
+ * Kotlin中的or关键字对应了Java中的|运算符
+ * xor关键字对应了Java中的^运算符
+ * and关键字其实就对应了Java中的&运算符
+ **/
+fun isDarkTheme(context: Context): Boolean {
+    val flag = context.resources.configuration.uiMode and
+            Configuration.UI_MODE_NIGHT_MASK
+    return flag == Configuration.UI_MODE_NIGHT_YES
+}
